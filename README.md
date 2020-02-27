d0c5a0f69097711b1be6c0e3792e24449e111d3b
Kiernan Brown <kwb6721@rit.edu>
2020-02-26 19:31:46 -0500

    Initial Version
    Added the initial version of the sandbox with a sample scene.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# ThirdPersonSandbox
-A third person sandbox in Unity to develop action game-like projects
+# Third Person Sandbox
+A third person sandbox in Unity to develop action game projects or experiment with action game elements.
+
+## Table of Contents
+* [Features](#features)
+* [Credits](#credits)
+
+## Features <a name="features"></a>
+* A sample scene where you can move a character around in third person and the camera follows you.
+* The foundation of a combo system. Left clicking allows you to attack, and doing attacks in succession allows you to do up to a four hit combo. You can stop for a brief period after an attack to restart the combo.
+![Imgur](https://i.imgur.com/qtuRnWL.gif)
+* The foundation of combo modifiers, which are moves that can be used to modify a combo, but can only be used mid combo. Right clicking after doing one of the first three hits in the combo allows you to perform a stomp, which restarts your combo.
+![Imgur](https://i.imgur.com/iGfz6VZ.gif)
+
+## Credits <a name="credits"></a>
+* Filmstorm - Third Person Camera Tutorial and Open World Movement System Tutorial. This helped to create the basic movement script for the character controller and helped with setup for the Cinemachine Third Person Camera.
+* Adobe Mixamo - The character XBot and all animations are from Mixamo.
---
+++ Third Person Sandbox/Assets/Scripts/Attack.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public bool actionable = true;
+    public bool canStomp = false;
+    public Animator anim;
+    public int state;
+    public MovementInput movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = this.GetComponent<MovementInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        state = anim.GetInteger("AttackState");
+        if (Input.GetButton("Fire1") && actionable == true)
+        {
+            StopCoroutine("Hit");
+            StopCoroutine("Stomp");
+            StartCoroutine("Hit");
+        }
+        if (Input.GetButton("Fire2") && actionable == true && canStomp)
+        {
+            StopCoroutine("Hit");
+            StartCoroutine("Stomp");
+        }
+    }
+
+    IEnumerator Hit()
+    {
+        if (anim.GetInteger("AttackState") == 0 || anim.GetInteger("AttackState") == 10)
+        {
+            movement.blockRotationPlayer = true;
+            anim.SetInteger("AttackState", 1);
+            DisableActions();
+            yield return new WaitForSeconds(0.3f);
+            actionable = true;
+            canStomp = true;
+            yield return new WaitForSeconds(0.5f);
+            RestartCombo();       
+        }
+        else if (anim.GetInteger("AttackState") == 1)
+        {
+            anim.SetInteger("AttackState", 2);
+            DisableActions();
+            yield return new WaitForSeconds(0.35f);
+            actionable = true;
+            canStomp = true;
+            yield return new WaitForSeconds(0.7f);
+            RestartCombo();
+        }
+        else if (anim.GetInteger("AttackState") == 2)
+        {
+            anim.SetInteger("AttackState", 3);
+            DisableActions();
+            yield return new WaitForSeconds(0.35f);
+            movement.verticalVel = 0.0325f;
+            yield return new WaitForSeconds(0.65f);
+            actionable = true;
+            yield return new WaitForSeconds(0.1f);
+            canStomp = true;
+            yield return new WaitForSeconds(0.4f);
+            RestartCombo();
+        }
+        else if (anim.GetInteger("AttackState") == 3)
+        {
+            anim.SetInteger("AttackState", 4);
+            DisableActions();
+            yield return new WaitForSeconds(0.75f);
+            anim.SetInteger("AttackState", 5);
+            yield return new WaitForSeconds(0.7f);
+            RestartCombo();
+        }
+    }
+
+    IEnumerator Stomp()
+    {
+        anim.SetInteger("AttackState", 10);
+        actionable = false;
+        yield return new WaitForSeconds(0.75f);
+        actionable = true;
+        yield return new WaitForSeconds(0.8f);
+        RestartCombo();
+    }
+
+    void RestartCombo()
+    {
+        anim.SetInteger("AttackState", 0);
+        actionable = true;
+        movement.blockRotationPlayer = false;
+        canStomp = false;
+    }
+
+    void DisableActions()
+    {
+        actionable = false;
+        canStomp = false;
+    }
+}
